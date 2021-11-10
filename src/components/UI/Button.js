f5a9e68e552efbdb9c01b9967207f76f975b1f14
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-10 12:34:43 +0630

    feat: Improve button UI

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,9 @@ import classes from './Button.module.css'
 
 const Button = (props) => {
     return (
-        <button className={classes.btn} onClick={props.clicked}>
+        <button
+            className={`${classes.btn} ${props.className}`}
+            onClick={props.clicked}>
             {props.text}
         </button>
     )
