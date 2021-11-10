ed83abe37870c9b04d52f797486cf9ca4625d6f6
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-10 12:31:30 +0630

    feat: Improve Model UI

--- src/components/Model/Model.js
+++ src/components/Model/Model.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import './Model.css'
+import classes from './Model.module.css'
 
 const model = (props) => {
     return (
-        <div className={'container'}>
-            <div className={'model'}>
-                <p>{props.text}</p>
-                <p>
-                    More info on{' '}
-                    <a href={props.link} target="_blank" rel="noreferrer">
-                        Wikipedia
-                    </a>
-                </p>
-                <button className={'btn'} onClick={props.clicked}>
-                    {props.buttonText}
-                </button>
-            </div>
+        <div className={`${classes.container} ${props.className}`}>
+            <div className={classes.model}>{props.children}</div>
         </div>
     )
 }
